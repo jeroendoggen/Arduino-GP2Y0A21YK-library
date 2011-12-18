404c2821a0683c5f2132ad26ca124381cdea28a3
jeroendoggen <jeroendoggen@gmail.com>
2011-12-18 23:51:30 +0000

    Added LGPL Copyright statements and licenses

--- DistanceGP2Y0A21YK.cpp
+++ DistanceGP2Y0A21YK.cpp
@@ -1,7 +1,22 @@
-/**********************************************************************************************************
- * DistanceGP2Y0A21YK.h - Library for retrieving data from the analog GP2Y0A21YK IR Distance sensor	  *
- * For more information: variable declaration, changelog,... see DistanceGP2Y0A21YK.h		          *
- *********************************************************************************************************/
+/************************************************************************************************************
+ * DistanceGP2Y0A21YK.h - Arduino library for retrieving data from the analog GP2Y0A21YK IR Distance sensor *
+ * Copyright 2011 Jeroen Doggen (jeroendoggen@gmail.com)                                                    *
+ * For more information: variable declaration, changelog,... see DistanceGP2Y0A21YK.h                       *
+ ************************************************************************************************************
+ * This library is free software; you can redistribute it and/or                                            *
+ * modify it under the terms of the GNU Lesser General Public                                               *
+ * License as published by the Free Software Foundation; either                                             *
+ * version 2.1 of the License, or (at your option) any later version.                                       *
+ *                                                                                                          *
+ * This library is distributed in the hope that it will be useful,                                          *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                                        *
+ * Lesser General Public License for more details.                                                          *
+ *                                                                                                          *
+ * You should have received a copy of the GNU Lesser General Public                                         *
+ * License along with this library; if not, write to the Free Software                                      *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA                               *
+ ***********************************************************************************************************/
 
 /// <summary>
 /// DistanceGP2Y0A21YK.cpp - Library for retrieving data from the GP2Y0A21YK IR Distance sensor.
